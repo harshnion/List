4efc992796f9f82d0c1da9d0b92bf5828cf9618d
harshnion <86431354+harshnion@users.noreply.github.com>
2021-06-26 23:34:56 +0530

    3rd commit

---
+++ List.py
@@ -0,0 +1,15 @@
+#Today we will make our 1st list in python
+
+# So Basically there are two methods of representin a list 
+
+
+# First one is to show all the items in one line and firstly we will make code for that. So that, it will be less confusing 
+Shopping = ["bread","milk","butter"]
+print(Shopping) 
+
+# The second method is to print each item one by one 
+#so let's watch how can we do that
+Shopping = ["bread","milk","butter"]
+for item in Shopping:
+  print(item)
+# Here, we use for loop for to print one item in one tym
